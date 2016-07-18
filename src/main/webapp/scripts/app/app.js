d711454a256bf2d46b198f89e0f4fd1b4b80a144
recoluan <1156743527@qq.com>
2016-07-18 14:16:05 +0800

    2016/07/18
    时间选择器

--- src/main/webapp/scripts/app/app.js
+++ src/main/webapp/scripts/app/app.js
@@ -16,82 +16,82 @@ angular.module('lepayglobleApp', ['LocalStorageModule',
                  $rootScope.personal = false;
              }
 
-             $rootScope.timePickerObj={
-                 "ranges": {
-                     "今天": [moment().format("YYYY/MM/DD 00:00:00"), moment().format("YYYY/MM/DD 23:59:59")],
-                     "昨天": [moment().subtract('days', 1).format("YYYY/MM/DD 00:00:00"), moment().subtract('days', 1).format("YYYY/MM/DD 23:59:59")],
-                     "最近七天": [moment().subtract('days', 6).format("YYYY/MM/DD 00:00:00"), moment().format("YYYY/MM/DD 23:59:59")],
-                     "最近30天": [moment().subtract('days', 29).format("YYYY/MM/DD 00:00:00"), moment().format("YYYY/MM/DD 23:59:59")]
-                 },
-                 "locale": {
-                     "direction": "ltr",
-                     "format": "YYYY/MM/DD HH:mm:ss",
-                     "separator": " - ",
-                     "applyLabel": "确认",
-                     "cancelLabel": "取消",
-                     "fromLabel": "从",
-                     "toLabel": "至",
-                     "customRangeLabel": "自定义",
-                     "daysOfWeek": [
-                         "日",
-                         "一",
-                         "二",
-                         "三",
-                         "四",
-                         "五",
-                         "六"
-                     ],
-                     "monthNames": [
-                         "一月",
-                         "二月",
-                         "三月",
-                         "四月",
-                         "五月",
-                         "六月",
-                         "七月",
-                         "八月",
-                         "九月",
-                         "十月",
-                         "十一月",
-                         "十二月"
-                     ],
-                     "firstDay": 1
-                 },
-                 "locale1": {
-                     "direction": "ltr",
-                     "format": "YYYY/MM/DD",
-                     "separator": " - ",
-                     "applyLabel": "确认",
-                     "cancelLabel": "取消",
-                     "fromLabel": "从",
-                     "toLabel": "至",
-                     "customRangeLabel": "自定义",
-                     "daysOfWeek": [
-                         "日",
-                         "一",
-                         "二",
-                         "三",
-                         "四",
-                         "五",
-                         "六"
-                     ],
-                     "monthNames": [
-                         "一月",
-                         "二月",
-                         "三月",
-                         "四月",
-                         "五月",
-                         "六月",
-                         "七月",
-                         "八月",
-                         "九月",
-                         "十月",
-                         "十一月",
-                         "十二月"
-                     ],
-                     "firstDay": 1
-                 }
-             }
+             // $rootScope.timePickerObj={
+             //     "ranges": {
+             //         "今天": [moment().format("YYYY/MM/DD 00:00:00"), moment().format("YYYY/MM/DD 23:59:59")],
+             //         "昨天": [moment().subtract('days', 1).format("YYYY/MM/DD 00:00:00"), moment().subtract('days', 1).format("YYYY/MM/DD 23:59:59")],
+             //         "最近七天": [moment().subtract('days', 6).format("YYYY/MM/DD 00:00:00"), moment().format("YYYY/MM/DD 23:59:59")],
+             //         "最近30天": [moment().subtract('days', 29).format("YYYY/MM/DD 00:00:00"), moment().format("YYYY/MM/DD 23:59:59")]
+             //     },
+             //     "locale": {
+             //         "direction": "ltr",
+             //         "format": "YYYY/MM/DD HH:mm:ss",
+             //         "separator": " - ",
+             //         "applyLabel": "确认",
+             //         "cancelLabel": "取消",
+             //         "fromLabel": "从",
+             //         "toLabel": "至",
+             //         "customRangeLabel": "自定义",
+             //         "daysOfWeek": [
+             //             "日",
+             //             "一",
+             //             "二",
+             //             "三",
+             //             "四",
+             //             "五",
+             //             "六"
+             //         ],
+             //         "monthNames": [
+             //             "一月",
+             //             "二月",
+             //             "三月",
+             //             "四月",
+             //             "五月",
+             //             "六月",
+             //             "七月",
+             //             "八月",
+             //             "九月",
+             //             "十月",
+             //             "十一月",
+             //             "十二月"
+             //         ],
+             //         "firstDay": 1
+             //     },
+             //     "locale1": {
+             //         "direction": "ltr",
+             //         "format": "YYYY/MM/DD",
+             //         "separator": " - ",
+             //         "applyLabel": "确认",
+             //         "cancelLabel": "取消",
+             //         "fromLabel": "从",
+             //         "toLabel": "至",
+             //         "customRangeLabel": "自定义",
+             //         "daysOfWeek": [
+             //             "日",
+             //             "一",
+             //             "二",
+             //             "三",
+             //             "四",
+             //             "五",
+             //             "六"
+             //         ],
+             //         "monthNames": [
+             //             "一月",
+             //             "二月",
+             //             "三月",
+             //             "四月",
+             //             "五月",
+             //             "六月",
+             //             "七月",
+             //             "八月",
+             //             "九月",
+             //             "十月",
+             //             "十一月",
+             //             "十二月"
+             //         ],
+             //         "firstDay": 1
+             //     }
+             // }
 
              $rootScope.ENV = ENV;
              $rootScope.VERSION = VERSION;
