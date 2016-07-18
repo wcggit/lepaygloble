9ff25b8457674a30b2dedc53ff21acb938d60505
recoluan <1156743527@qq.com>
2016-07-18 14:32:27 +0800

    20160718

--- src/main/webapp/scripts/app/merchant/trademanage/orderList.controller.js
+++ src/main/webapp/scripts/app/merchant/trademanage/orderList.controller.js
@@ -3,24 +3,40 @@
 angular.module('lepayglobleApp')
     .controller('OrderListController',
                 function ($scope, $state, $location, Trade, $rootScope, $stateParams) {
-                    $('#timePicker1').daterangepicker({
-                                                          "autoApply": true,
-                                                          "showDropdowns": true,
-                                                          "showWeekNumbers": true,
-                                                          "showISOWeekNumbers": true,
-                                                          "timePicker": true,
-                                                          "timePicker24Hour": true,
-                                                          "timePickerSeconds": true,
-                                                          "ranges": $rootScope.timePickerObj.ranges,
-                                                          "locale": $rootScope.timePickerObj.locale,
-                                                          "alwaysShowCalendars": true,
-                                                          "startDate": moment().subtract(1,
-                                                                                         'day').format("YYYY/MM/DD 00:00:00"),
-                                                          "endDate": moment().subtract(1,
-                                                                                       'day').format("YYYY/MM/DD 23:59:59"),
-                                                          "opens": "right"
-                                                      }, function (start, end, label) {
-                    });
+                    // $('#timePicker1').daterangepicker({
+                    //       "autoApply": true,
+                    //       "showDropdowns": true,
+                    //       "showWeekNumbers": true,
+                    //       "showISOWeekNumbers": true,
+                    //       "timePicker": true,
+                    //       "timePicker24Hour": true,
+                    //       "timePickerSeconds": true,
+                    //       "ranges": $rootScope.timePickerObj.ranges,
+                    //       "locale": $rootScope.timePickerObj.locale,
+                    //       "alwaysShowCalendars": true,
+                    //       "startDate": moment().subtract(1,
+                    //                                      'day').format("YYYY/MM/DD 00:00:00"),
+                    //       "endDate": moment().subtract(1,
+                    //                                    'day').format("YYYY/MM/DD 23:59:59"),
+                    //       "opens": "right"
+                    //   }, function (start, end, label) {
+                    //    
+                    //   });
+                    $('#timePicker1')
+                        .val(moment().subtract('day', 1).format('YYYY/MM/DD HH:mm:00') + ' - ' + moment().format('YYYY/MM/DD HH:mm:59'))
+                        .daterangepicker({
+                            timePicker: true, //是否显示小时和分钟
+                            timePickerIncrement: 1, //时间的增量，单位为分钟
+                            opens : 'left', //日期选择框的弹出位置
+                            format : 'YYYY/MM/DD HH:mm:ss', //控件中from和to 显示的日期格式
+                            ranges : {
+                                '最近1小时': [moment().subtract('hours',1), moment()],
+                                '今日': [moment().startOf('day'), moment()],
+                                '昨日': [moment().subtract('days', 1).startOf('day'), moment().subtract('days', 1).endOf('day')],
+                                '最近7日': [moment().subtract('days', 6), moment()],
+                                '最近30日': [moment().subtract('days', 29), moment()]
+                            },
+                        },function(start, end, label) {});
                     $("#timePicker1").val("");
                     var currentPage = 1;
                     var olOrderCriteria = {};
--- src/main/webapp/scripts/app/merchant/userrealize/member.controller.js
+++ src/main/webapp/scripts/app/merchant/userrealize/member.controller.js
@@ -1,25 +1,37 @@
 angular.module('lepayglobleApp')
     .controller('MemberController', function ($scope, $state, Commission, $q, $rootScope) {
 
-                    $('#timePicker6').daterangepicker({
-                                                          "autoApply": true,
-                                                          "showDropdowns": true,
-                                                          "showWeekNumbers": true,
-                                                          "showISOWeekNumbers": true,
-                                                          "timePicker": true,
-                                                          "timePicker24Hour": true,
-                                                          "timePickerSeconds": true,
-                                                          "ranges": $rootScope.timePickerObj.ranges,
-                                                          "locale": $rootScope.timePickerObj.locale,
-                                                          "alwaysShowCalendars": true,
-                                                          "startDate": moment().subtract(1,
-                                                                                         'day').format("YYYY/MM/DD 00:00:00"),
-                                                          "endDate": moment().subtract(1,
-                                                                                       'day').format("YYYY/MM/DD 23:59:59"),
-                                                          "opens": "right"
-                                                      }, function (start, end, label) {
-                    });
-                    $("#timePicker6").val("");
+                    // $('#timePicker1').daterangepicker({
+                    //     "autoApply": true,
+                    //     "showDropdowns": true,
+                    //     "showWeekNumbers": true,
+                    //     "showISOWeekNumbers": true,
+                    //     "timePicker": true,
+                    //     "timePicker24Hour": true,
+                    //     "timePickerSeconds": true,
+                    //     "ranges": $rootScope.timePickerObj.ranges,
+                    //     "locale": $rootScope.timePickerObj.locale,
+                    //     "alwaysShowCalendars": true,
+                    //     "startDate": moment().subtract(1,'day').format("YYYY/MM/DD 00:00:00"),
+                    //     "endDate": moment().subtract(1,'day').format("YYYY/MM/DD 23:59:59"),
+                    //     "opens": "right"
+                    // }, function (start, end, label) {});
+                    // $("#timePicker1").val("");
+                    $('#timePicker1')
+                        .val(moment().subtract('day', 1).format('YYYY/MM/DD HH:mm:00') + ' - ' + moment().format('YYYY/MM/DD HH:mm:59'))
+                        .daterangepicker({
+                            timePicker: true, //是否显示小时和分钟
+                            timePickerIncrement: 1, //时间的增量，单位为分钟
+                            opens : 'left', //日期选择框的弹出位置
+                            format : 'YYYY/MM/DD HH:mm:ss', //控件中from和to 显示的日期格式
+                            ranges : {
+                                '最近1小时': [moment().subtract('hours',1), moment()],
+                                '今日': [moment().startOf('day'), moment()],
+                                '昨日': [moment().subtract('days', 1).startOf('day'), moment().subtract('days', 1).endOf('day')],
+                                '最近7日': [moment().subtract('days', 6), moment()],
+                                '最近30日': [moment().subtract('days', 29), moment()]
+                            },
+                        },function(start, end, label) {});
 
                     var currentPage = 1;
                     var olOrderCriteria = {};
