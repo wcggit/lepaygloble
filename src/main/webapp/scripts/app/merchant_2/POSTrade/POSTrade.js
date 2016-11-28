7c4f84f356c8f7501b7e5c44263caf81e8774ceb
Reco Luan <1156743527@qq.com>
2016-11-28 09:58:20 +0800

    20161128

--- src/main/webapp/scripts/app/merchant_2/POSTrade/POSTrade.js
+++ src/main/webapp/scripts/app/merchant_2/POSTrade/POSTrade.js
@@ -13,9 +13,54 @@ angular.module('lepayglobleApp')
                     'right-content@merchant_2': {
                         templateUrl: 'scripts/app/merchant_2/POSTrade/POSTrade.html',
                         controller: 'POSTradeController'
+                    },
+                    'POSTradeTab@POSTrade': {
+                        templateUrl: 'scripts/app/merchant_2/POSTrade/tradeRecord.html',
+                        controller: 'tradeRecordController'
                     }
                 },
-                resolve: {
-                }
+                resolve: {}
+            })
+            .state('tradeRecord', {
+                parent: 'POSTrade',
+                url: '/tradeRecord',
+                data: {
+                    authorities: ["merchant"]
+                },
+                views: {
+                    'POSTradeTab@POSTrade': {
+                        templateUrl: 'scripts/app/merchant_2/POSTrade/tradeRecord.html',
+                        controller: 'tradeRecordController'
+                    }
+                },
+                resolve: {}
+            })
+            .state('myPOSTrade', {
+                parent: 'POSTrade',
+                url: '/myPOSTrade',
+                data: {
+                    authorities: ["merchant"]
+                },
+                views: {
+                    'POSTradeTab@POSTrade': {
+                        templateUrl: 'scripts/app/merchant_2/POSTrade/myPOSTrade.html',
+                        controller: 'myPOSTradeController'
+                    }
+                },
+                resolve: {}
+            })
+            .state('myPOSEmpty', {
+                parent: 'POSTrade',
+                url: '/myPOSEmpty',
+                data: {
+                    authorities: ["merchant"]
+                },
+                views: {
+                    'POSTradeTab@POSTrade': {
+                        templateUrl: 'scripts/app/merchant_2/POSTrade/myPOSEmpty.html',
+                        controller: 'myPOSEmptyController'
+                    }
+                },
+                resolve: {}
             });
     });
--- src/main/webapp/scripts/app/merchant_2/homePage/homePage.js
+++ src/main/webapp/scripts/app/merchant_2/homePage/homePage.js
@@ -17,5 +17,20 @@ angular.module('lepayglobleApp')
                 },
                 resolve: {
                 }
-            });
+            })
+            .state('withdrawRecord', {
+                parent: 'merchant_2',
+                url: '/withdrawRecord',
+                data: {
+                    authorities: ["merchant"]
+                },
+                views: {
+                    'right-content@merchant_2': {
+                        templateUrl: 'scripts/app/merchant_2/homePage/withdrawRecord.html',
+                        controller: 'withdrawRecordController'
+                    }
+                },
+                resolve: {
+                }
+             });
     });
