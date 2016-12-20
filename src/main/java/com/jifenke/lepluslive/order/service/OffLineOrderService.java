aa802452c59fe7c61f3df54a3fc53807e77af620
zhaiyu <42427106@qq.com>
2016-12-20 11:10:45 +0800

    2016-12-20

--- src/main/java/com/jifenke/lepluslive/order/service/OffLineOrderService.java
+++ src/main/java/com/jifenke/lepluslive/order/service/OffLineOrderService.java
@@ -1,6 +1,7 @@
 package com.jifenke.lepluslive.order.service;
 
 import com.jifenke.lepluslive.merchant.domain.criteria.CodeOrderCriteria;
+import com.jifenke.lepluslive.merchant.domain.criteria.CommissionDetailsCriteria;
 import com.jifenke.lepluslive.merchant.domain.entities.Merchant;
 import com.jifenke.lepluslive.order.domain.criteria.FinancialCriteria;
 import com.jifenke.lepluslive.order.domain.criteria.OLOrderCriteria;
@@ -522,6 +523,79 @@ public class OffLineOrderService {
     @Transactional(propagation = Propagation.REQUIRED, readOnly = true)
     public List<Object[]> findMyCodePriceByMerchantid(Long merchantId) {
         return offLineOrderRepository.findMyCodePriceByMerchantid(merchantId);
+    }
+
+    /**
+     *  查询 线下 佣金明细
+     * @param commissionDetailsCriteria
+     * @return
+     */
+    @Transactional(propagation = Propagation.REQUIRED, readOnly = true)
+    public List getCommissionDetailsList_off_line(CommissionDetailsCriteria commissionDetailsCriteria) {
+
+        int start = commissionDetailsCriteria.getPageSize() * (commissionDetailsCriteria.getCurrentPage() - 1);
+
+        StringBuffer sql = new StringBuffer();
+        sql.append("select  olo_olos.complete_date, wxu.nickname, wxu.head_image_url, merchant.name, olo_olos.total_price, olo_olos.true_pay, olo_olos.share_ from "
+                   + " (select olo.complete_date as complete_date,  olo.le_jia_user_id as user_id,  olos.lock_merchant_id as merchant_id, "
+                   + " olo.total_price as total_price,  olo.true_pay as true_pay,  olos.to_lock_merchant as share_ "
+                   + " from off_line_order olo, off_line_order_share olos where olo.id = olos.off_line_order_id and olo.state = 1 ");
+        if (commissionDetailsCriteria.getStoreIds() != null) {
+            sql.append(" and olos.lock_merchant_id in (");
+            for(int i =0;i<commissionDetailsCriteria.getStoreIds().length;i++){
+                sql.append(commissionDetailsCriteria.getStoreIds()[i]+",");
+            }
+            sql.deleteCharAt(sql.length()-1);
+            sql.append(")");
+        }
+        if (commissionDetailsCriteria.getStartDate() != null && commissionDetailsCriteria.getStartDate() != "") {
+            sql.append(" and olo.complete_date between '");
+            sql.append(commissionDetailsCriteria.getStartDate());
+            sql.append("' and '");
+            sql.append(commissionDetailsCriteria.getEndDate());
+        }
+        sql.append(" order by olo.complete_date desc limit ");
+        sql.append(start);
+        sql.append(",");
+        sql.append(commissionDetailsCriteria.getPageSize());
+
+        sql.append(" ) as olo_olos  left join merchant on olo_olos.merchant_id = merchant.id ");
+        sql.append(" left join wei_xin_user wxu on olo_olos.user_id = wxu.le_jia_user_id ");
+
+        Query query = em.createNativeQuery(sql.toString());
+        List<Object[]> details = query.getResultList();
+        return details;
+    }
+
+    /**
+     *  查询 线下 佣金明细count
+     * @param commissionDetailsCriteria
+     * @return
+     */
+    @Transactional(propagation = Propagation.REQUIRED, readOnly = true)
+    public List getCommissionDetails_off_line_count(CommissionDetailsCriteria commissionDetailsCriteria) {
+
+        StringBuffer sql = new StringBuffer();
+        sql.append(" select count(olo.id), sum(olos.to_lock_merchant) "
+                   + " from off_line_order olo, off_line_order_share olos where olo.id = olos.off_line_order_id and olo.state = 1 ");
+        if (commissionDetailsCriteria.getStoreIds() != null) {
+            sql.append(" and olos.lock_merchant_id in (");
+            for(int i =0;i<commissionDetailsCriteria.getStoreIds().length;i++){
+                sql.append(commissionDetailsCriteria.getStoreIds()[i]+",");
+            }
+            sql.deleteCharAt(sql.length()-1);
+            sql.append(")");
+        }
+        if (commissionDetailsCriteria.getStartDate() != null && commissionDetailsCriteria.getStartDate() != "") {
+            sql.append(" and olo.complete_date between '");
+            sql.append(commissionDetailsCriteria.getStartDate());
+            sql.append("' and '");
+            sql.append(commissionDetailsCriteria.getEndDate());
+        }
+
+        Query query = em.createNativeQuery(sql.toString());
+        List<Object[]> details = query.getResultList();
+        return details;
     }
 
 }
