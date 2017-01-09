ba5d7e642d98441f8d31dde1cb99bd3f5113c2d4
liangshuangshun4 <2170951923@qq.com>
2017-01-09 18:06:30 +0800

    提现限制

--- src/main/java/com/jifenke/lepluslive/withdraw/repository/WithdrawRepository.java
+++ src/main/java/com/jifenke/lepluslive/withdraw/repository/WithdrawRepository.java
@@ -6,6 +6,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 /**
  * Created by xf on 2016/9/18.
@@ -13,5 +16,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface WithdrawRepository extends JpaRepository<WithdrawBill, Long> {
 
     Page findAll(Specification<WithdrawBill> specification,Pageable pageRequest);
+    @Query(value = "SELECT * from withdraw_bill WHERE merchant_id=?1 and state=0", nativeQuery = true)
+    List<WithdrawBill> findByMerchantId(Long id);
 
+    @Query(value = "SELECT * from withdraw_bill WHERE partner_id=?1 and state=0", nativeQuery = true)
+    List<WithdrawBill> findByPartnerId(Long id);
 }
