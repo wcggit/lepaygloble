47701551d009faff76145daf2346e1abb067413d
Wang-HL <2570578094@qq.com>
2017-12-18 15:26:20 +0800

    前端代码备份

--- src/main/webapp/scripts/app/merchant_2/tradeManage/codeTrade/refund.controller.js
+++ src/main/webapp/scripts/app/merchant_2/tradeManage/codeTrade/refund.controller.js
@@ -6,5 +6,32 @@
 
 angular.module('lepayglobleApp')
     .controller('refundController', function ($scope, $state, $rootScope, $location,Principal,Auth,$http) {
+        // 订单类型
+        var orderType;
 
+        // 查询订单
+        $scope.searchByCriteria = function () {
+            var orderId = $("#orderSid").val();
+            if(orderId != ''){
+                $http.get("/api/channel/refund/" + orderId).success(function (response) {
+                    if (response.status == 200) {
+                        var data = response.data;
+                        console.log()
+                    } else {
+                        alert("订单号错误...");
+                    }
+                });
+                $(".refundInfo").fadeIn();
+            }
+        };
+
+        // 确认
+        $scope.sub = function () {
+
+        };
+
+        // 取消
+        $scope.cancel = function () {
+            $(".refundInfo").fadeOut();
+        };
     });
